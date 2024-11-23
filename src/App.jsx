34210e4d1c5c5ffbe2f6078f54198118714e53e4
its-kronos <anseld41@gmail.com>
2024-11-23 16:40:39 -0500

    basic vite react setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import H1 from "./components/H1"
+
+function App() {
+  return <H1 text="hello!" />
+}
+
+export default App
---
+++ src/components/H1.jsx
@@ -0,0 +1,5 @@
+function H1({ text }) {
+    return <h1>{text}</h1>
+}
+
+export default H1
